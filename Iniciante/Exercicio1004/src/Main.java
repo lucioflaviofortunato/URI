e53a3d9222a9ff3a47b2000282ee0b9369018168
Lucio Flavio <luciofortunato1985@gmail.com>
2019-10-30 14:41:49 -0400

    commit alteracao final

--- Iniciante/Exercicio1004/src/Main.java
+++ Iniciante/Exercicio1004/src/Main.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int a,b,prod;
-		
-		Scanner input =new Scanner(System.in);
-		
-		a = input.nextInt();
-		b = input.nextInt();
-		
-		prod = a*b;
-		
-		System.out.println("PROD = "+prod);
+		int a, b, prod;
 
+		try (Scanner input = new Scanner(System.in)) {
+
+			a = input.nextInt();
+			b = input.nextInt();
+
+			prod = a * b;
+
+			System.out.println("PROD = " + prod);
+		}
 	}
 
 }
-
