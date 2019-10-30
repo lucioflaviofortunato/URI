1c1c987a92747a3698735b86f67012f126595869
Lucio Flavio <luciofortunato1985@gmail.com>
2019-10-30 14:41:18 -0400

    commit final resolucao

---
+++ Iniciante/Exercicio1004/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a,b,prod;
+		
+		Scanner input =new Scanner(System.in);
+		
+		a = input.nextInt();
+		b = input.nextInt();
+		
+		prod = a*b;
+		
+		System.out.println("PROD = "+prod);
+
+	}
+
+}
+
